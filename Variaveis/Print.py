3f9674dca4009660d1da3817c1a630bd8b0641dd
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-03-28 15:38:37 -0300

    Dia 1 - Função print

---
+++ Variaveis/Print.py
@@ -0,0 +1,7 @@
+#Função print -> imprime algo na tela 
+print(1, 2, 3)
+
+#sep -> define como as coisas vão ser separadas dentro do print (por padrão separa com o espaço ' ')
+print(1, 2, 3, sep='-')   
+#end -> define o que acontece após o print (por podrão quebra a linha '\n')
+print(1, 2, 3, end=' OK\n')
