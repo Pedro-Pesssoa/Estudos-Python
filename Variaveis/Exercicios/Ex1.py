1e2a7b1ba23c4fc9a28b6f953dae4a30ad6dfaad
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-03-28 21:23:48 -0300

    Exercicio 1

---
+++ Variaveis/Exercicios/Ex1.py
@@ -0,0 +1,14 @@
+nome = 'Pedro'
+sobrenome = 'Pessoa'
+ano_nasc = 2002
+idade = 2023 - ano_nasc
+maior_idade = idade >= 18
+altura = 1.70
+peso = 67.0
+
+print('Nome:', nome, end=' - ')
+print('Sobrenome:', sobrenome)
+print('Altura:', altura, end=' - ')
+print('Peso', peso)
+print('Idade:', idade, end=' - ')
+print('Maior de idade:', maior_idade)
