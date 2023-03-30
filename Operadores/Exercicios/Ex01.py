836e6bd191e75ddc5400eb9a98985a127982f90b
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-03-29 23:37:30 -0300

    exercico 1 - operadores aritimeticos

---
+++ Operadores/Exercicios/Ex01.py
@@ -0,0 +1,9 @@
+#Calculo de IMC -> peso/(altura*altura)
+nome = 'Pedro'
+peso = 67
+altura = 1.70
+
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura,', peso, 'de peso')
+print('Seu IMC é', imc)
