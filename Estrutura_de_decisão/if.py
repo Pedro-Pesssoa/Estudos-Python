d944372740d66386204b6bdac919cd4c063d061c
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-03-30 18:17:02 -0300

    Dia 4 - Estrutura de decisão

---
+++ Estrutura_de_decisão/if.py
@@ -0,0 +1,12 @@
+opt = input('Digite 1 ou 2: ')
+int_opt = int(opt)
+
+#if comparação :
+if int_opt == 1:
+    print('Você digitou 1')
+#elif comparação :
+if int_opt == 2:
+    print('Você digitou 2')
+#else :
+else :
+    print('Você não digitou nem 1 nem 2')
