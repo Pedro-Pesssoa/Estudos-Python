4c51d0331dc8e6486e3c227f45a9a66d90e050c2
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-03-30 00:08:09 -0300

    Dia 3 - f-string

---
+++ Formatacao/f_string.py
@@ -0,0 +1,9 @@
+nome = 'Pedro'
+idade = 21
+altura = 1.70
+dinheiro = 30000.50
+
+print(f'{nome} tem {idade} anos e mede {altura:.2f} ele tem {dinheiro:,.2f} reais')
+
+#Com :.valorf é possivel definir quantas casas descimais vão aparecer 
+#Ao acrecentar a , caso o numero for grande ele séra separado por virgulas
